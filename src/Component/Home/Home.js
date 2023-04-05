e0aa7887aa4ca16a4eba7ba9ea017c55e20d0d3d
ismail360deg <ismailbhuiyan2000@gmail.com>
2023-04-05 11:39:15 +0600

    choice partdesign added

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import CareerGig from "../CareerGig/CareerGig";
 import ChooseUs from "../ChooseUs/ChooseUs";
 import Banner from "../Banner/Banner";
+import Choice from "../Choice/Choice";
 
 const Home = () => {
   return (
     <div>
       <Banner/>
       <CareerGig />
+      <Choice></Choice>
       <ChooseUs></ChooseUs>
     </div>
   );
