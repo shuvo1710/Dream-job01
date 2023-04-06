d2afc5c92eb9ec94c3c68819173e500d4b16efa4
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2023-04-06 05:43:08 -0400

    added context api and login method

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,26 +3,32 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Component/LayOut/Main";
 import Home from "../Component/Home/Home";
 import Features from "../Component/Features/Features";
+import SignIn from "../Component/SignIn/SignIn";
+import SignUp from "../SignUp/SignUp";
 
 export const router = createBrowserRouter([
-
-
-
-    {
-        path:'/',
-        element:<Main/>,
-        children:[
-            {
-                path:"/",
-                element:<Home/>
-            },
-              {
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
         path: "/feature",
         element: <Features />,
       },
-            
-        ]
-       
-    }
-])
+    ],
+  },
 
+  {
+    path: '/signin',
+    element: <SignIn/>
+  },
+  {
+    path: '/signup',
+    element: <SignUp/>
+  }
+
+]);
