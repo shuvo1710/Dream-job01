2c38475dd587ea1e0e124b57daebaa55e6c9fb86
yeasinarafat51 <aloondokar65@gmail.com>
2023-04-06 23:56:25 +0600

    what2

--- src/Component/all/All.js
+++ src/Component/all/All.js
@@ -15,8 +15,8 @@ const All = () => {
                 <img src={Rectangle9} alt=""/>
 
             </div>
-            <div className='my-4 w-50%'>
-                <h1 className='text-2xl text-[#000000] mb-4 ml-4 '>All careers gigs, one portal</h1>
+            <div className='my-4 w-50% ml-4'>
+                <h1 className='text-2xl text-[#000000] mb-4  '>All careers gigs, one portal</h1>
                 <p className='text-[16px] leading-[19px] font-[400] text-[#011025] '>Whatever you are, a software developer, a user interface designer or <br /> someone else, we got you. We have all types of jobs ready for you.</p>
                 <button className='bg-cyan-600 text-[#FFFFFF] text-[16px] leading-[19px] font-[400] p-4 my-4'>Show me all available jobs</button>
             </div>
