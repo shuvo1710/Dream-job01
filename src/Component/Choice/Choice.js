9c154b50fd78bd6076fe4f38a22cb62701438ca3
ismail360deg <ismailbhuiyan2000@gmail.com>
2023-04-07 16:32:55 +0600

    choice part some changes

--- src/Component/Choice/Choice.js
+++ src/Component/Choice/Choice.js
@@ -30,96 +30,13 @@ const Choice = () => {
 				</div>
 				<div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
 
-					<article className="flex flex-col bg-[#F2F6FC] hover:bg-[#011025] duration-300">
-						<div className="flex flex-col flex-1 p-6 group/text-card">
-							<TbDiscount2Off className='choose__icon'></TbDiscount2Off>
-							<h3 className="flex-1 py-2 text-lg font-semibold leading-snug text-gray-700 group-hover/text-card:text-[#C8C000]">Web development</h3>
-							<div className="flex flex-wrap justify-between space-x-2 text-xs text-gray-300">
-								<span className='text-[#999999]'>Would you say no to somebody who want
-									to help you land a job? That’s impossible.
-								</span>
-							</div>
-						</div>
-					</article>
-					<article className="flex flex-col bg-[#F2F6FC] hover:bg-[#011025] duration-300">
-						<div className="flex flex-col flex-1 p-6 group/text-card">
-							<TbDiscount2Off className='choose__icon'></TbDiscount2Off>
-							<h3 className="flex-1 py-2 text-lg font-semibold leading-snug text-gray-700 group-hover/text-card:text-[#C8C000]">Product designer</h3>
-							<div className="flex flex-wrap justify-between space-x-2 text-xs text-gray-300">
-								<span className='text-[#999999]'>Would you say no to somebody who want
-									to help you land a job? That’s impossible.
-								</span>
-							</div>
-						</div>
-					</article>
-					<article className="flex flex-col bg-[#F2F6FC] hover:bg-[#011025] duration-300">
-						<div className="flex flex-col flex-1 p-6 group/text-card">
-							<TbDiscount2Off className='choose__icon'></TbDiscount2Off>
-							<h3 className="flex-1 py-2 text-lg font-semibold leading-snug text-gray-700 group-hover/text-card:text-[#C8C000]">Financial accountant</h3>
-							<div className="flex flex-wrap justify-between space-x-2 text-xs text-gray-300">
-								<span className='text-[#999999]' >Would you say no to somebody who want
-									to help you land a job? That’s impossible.
-								</span>
-							</div>
-						</div>
-					</article>
-					<article className="flex flex-col bg-[#F2F6FC] hover:bg-[#011025] duration-300">
-						<div className="flex flex-col flex-1 p-6 group/text-card">
-							<TbDiscount2Off className='choose__icon'></TbDiscount2Off>
-							<h3 className="flex-1 py-2 text-lg font-semibold leading-snug text-gray-700 group-hover/text-card:text-[#C8C000]">Civil engineer</h3>
-							<div className="flex flex-wrap justify-between space-x-2 text-xs text-gray-300">
-								<span className='text-[#999999]'>Would you say no to somebody who want
-									to help you land a job? That’s impossible.
-								</span>
-							</div>
-						</div>
-					</article>
-					<article className="flex flex-col bg-[#F2F6FC] hover:bg-[#011025] duration-300">
-						<div className="flex flex-col flex-1 p-6 group/text-card">
-							<TbDiscount2Off className='choose__icon'></TbDiscount2Off>
-							<h3 className="flex-1 py-2 text-lg font-semibold leading-snug text-gray-700 group-hover/text-card:text-[#C8C000]">Electrician</h3>
-							<div className="flex flex-wrap justify-between space-x-2 text-xs text-gray-300">
-								<span className='text-[#999999]'>Would you say no to somebody who want
-									to help you land a job? That’s impossible.
-								</span>
-							</div>
-						</div>
-					</article>
-					<article className="flex flex-col bg-[#F2F6FC] hover:bg-[#011025] duration-300">
-
-						<div className="flex flex-col flex-1 p-6 group/text-card">
-							<TbDiscount2Off className='choose__icon'></TbDiscount2Off>
-							<h3 className="flex-1 py-2 text-lg font-semibold leading-snug text-gray-700 group-hover/text-card:text-[#C8C000]">Web development</h3>
-							<div className="flex flex-wrap justify-between space-x-2 text-xs text-gray-300">
-								<span className='text-[#999999]'>Would you say no to somebody who want
-									to help you land a job? That’s impossible.
-								</span>
-							</div>
-						</div>
-
-					</article>
-					<article className="flex flex-col bg-[#F2F6FC] hover:bg-[#011025] duration-300">
-						<div className="flex flex-col flex-1 p-6 group/text-card">
-							<TbDiscount2Off className='choose__icon'></TbDiscount2Off>
-							<h3 className="flex-1 py-2 text-lg font-semibold leading-snug text-gray-700 group-hover/text-card:text-[#C8C000]">Product designer</h3>
-							<div className="flex flex-wrap justify-between space-x-2 text-xs text-gray-300">
-								<span className='text-[#999999]'>Would you say no to somebody who want
-									to help you land a job? That’s impossible.
-								</span>
-							</div>
-						</div>
-					</article>
-					<article className="flex flex-col bg-[#F2F6FC] hover:bg-[#011025] duration-300">
-						<div className="flex flex-col flex-1 p-6 group/text-card">
-							<TbDiscount2Off className='choose__icon'></TbDiscount2Off>
-							<h3 className="flex-1 py-2 text-lg font-semibold leading-snug text-gray-700 group-hover/text-card:text-[#C8C000]">Electrician</h3>
-							<div className="flex flex-wrap justify-between space-x-2 text-xs text-gray-300">
-								<span className='text-[#999999]'>Would you say no to somebody who want
-									to help you land a job? That’s impossible.
-								</span>
-							</div>
-						</div>
-					</article>
+				{
+                jobNames.map(jobName => <ChoiceInfos
+                    key={jobName._id}
+                    jobName={jobName}
+                ></ChoiceInfos>)
+                }
+					
 				</div>
 			</div>
 		</section>
