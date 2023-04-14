163298562824b3d651ad3415268b41c73fdb4dfa
yeasinarafat51 <aloondokar65@gmail.com>
2023-04-15 00:10:02 +0600

    price1

--- src/price/Price.js
+++ src/price/Price.js
@@ -12,7 +12,7 @@ const Price = () => {
             <div className='ml-8'><h1  className='text-[#112211] text-xl'>price</h1>
             <div className="form-control">
   <div className="input-group">
-    <input type="text" placeholder="price…" className="input input-bordered" />
+    <input type="text" placeholder="price…." className="input input-bordered" />
     <button className="btn btn-success ml-5">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
     </button>
@@ -33,7 +33,7 @@ const Price = () => {
     <li>Free breakfast</li>
     <li>Free parking</li>
     <li>Free internet</li>
-    <li>Free airport shuttle</li>
+    <li>Free airport shuttlee</li>
     <li>Free cancellation</li>
   </div>
   <div className="divider"></div> 
